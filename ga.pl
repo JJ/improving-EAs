b6fe412911fa8c0f3c6509d859f0e148df26403e
jmerelo <jmerelo@leonard>
2010-05-03 17:44:49 +0200

    Eliminación de un bucle para cálculo del fitness total

--- ga.pl
+++ ga.pl
@@ -18,45 +18,44 @@ my @population = map( random_chromosome( $chromosome_length ),
 		      1..$population_size );
 
 my %fitness_of;
-map( compute_fitness( $_ ), @population );
 my ($this_generation,@best);
 do {
-  my @sorted_population = sort { $fitness_of{$b} 
+    my $total_fitness = 0;
+    map( ((!$fitness_of{$_})?compute_fitness( $_ ):1) 
+	 && ( $total_fitness += $fitness_of{$_} ), @population );
+    my @sorted_population = sort { $fitness_of{$b} 
 				   <=> $fitness_of{$a} } @population;
-  @best = @sorted_population[0,1];
-#  print $best[0], " ", $fitness_of{$best[0]}, "\n";
-  my $wheel = compute_wheel( \@sorted_population );
-  my @slots = spin( $wheel, $population_size );
-  my @pool;
-  my $index = 0;
-  do {
-    my $p = $index++ % @slots;
-    my $copies = $slots[$p];
-    for (1..$copies) {
-      push @pool, $sorted_population[$p];
-    }
-  } while ( @pool <= $population_size );
-
-  @population = ();
-  map( $_ = mutate($_), @pool );
-  for ( my $i = 0; $i < $population_size/2 -1 ; $i++ )  {
-    my $first = $pool[rand($#pool)];
-    my $second = $pool[rand($#pool)];
+    @best = @sorted_population[0,1];
+#    print $best[0], " ", $fitness_of{$best[0]}, "\n";
+    my @wheel = map( $fitness_of{$_}/$total_fitness, @sorted_population);
+    my @slots = spin( \@wheel, $population_size );
+    my @pool;
+    my $index = 0;
+    do {
+	my $p = $index++ % @slots;
+	my $copies = $slots[$p];
+	for (1..$copies) {
+	    push @pool, $sorted_population[$p];
+	}
+    } while ( @pool <= $population_size );
     
-    push @population, crossover( $first, $second );
-  }
- 
-  map( (!$fitness_of{$_})?compute_fitness( $_ ):1, @population );
-  push @population, @best;
+    @population = ();
+    map( $_ = mutate($_), @pool );
+    for ( my $i = 0; $i < $population_size/2 -1 ; $i++ )  {
+	my $first = $pool[rand($#pool)];
+	my $second = $pool[rand($#pool)];
+	
+	push @population, crossover( $first, $second );
+    }
+    
+    push @population, @best;
 } while ( ( $this_generation++ < $generations ) &&
 	  ($fitness_of{$best[0]} < $chromosome_length ) );
 
 sub compute_wheel {
-  my $population = shift;
-  my $total_fitness;
-  map( $total_fitness += $fitness_of{$_}, @$population );
-  my @wheel = map( $fitness_of{$_}/$total_fitness, @$population);
-  return \@wheel;
+    my ($population,$total_fitness) = @_;
+    my @wheel = map( $fitness_of{$_}/$total_fitness, @$population);
+    return \@wheel;
 }
 
 sub spin {
