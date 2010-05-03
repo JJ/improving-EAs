ae97721bab61fcfb3d03f4622c6a1785e3308f02
jmerelo <jmerelo@leonard>
2010-05-03 17:27:51 +0200

    Primera mejora, con caché

--- ga.pl
+++ ga.pl
@@ -1,8 +1,5 @@
 #!/usr/bin/perl
 
-use strict;
-use warnings;
-
 my $chromosome_length = shift || 16;
 my $population_size = shift || 32;
 my $generations = shift || 100;
@@ -16,11 +13,13 @@ EOC
 my @population = map( random_chromosome( $chromosome_length ), 
 		      1..$population_size );
 
+my %fitness_of;
 map( compute_fitness( $_ ), @population );
 for ( 1..$generations ) {
-  my @sorted_population = sort { $b->{'fitness'} <=> $a->{'fitness'} } @population;
-  my @best = @sorted_population[0,1]; # Keep for later
-  print $best[0]->{'string'}, " ", $best[0]->{'fitness'}, "\n";
+  my @sorted_population = sort { $fitness_of{$b} 
+				   <=> $fitness_of{$a} } @population;
+  my @best = @sorted_population[0,1];
+  print $best[0], " ", $fitness_of{$best[0]}, "\n";
   my @wheel = compute_wheel( \@sorted_population );
   my @slots = spin( \@wheel, $population_size );
   my @pool;
@@ -34,7 +33,7 @@ for ( 1..$generations ) {
   } while ( @pool <= $population_size );
 
   @population = ();
-  map( mutate($_), @pool );
+  map( $_ = mutate($_), @pool );
   for ( my $i = 0; $i < $population_size/2 -1 ; $i++ )  {
     my $first = $pool[rand($#pool)];
     my $second = $pool[ rand($#pool)];
@@ -48,8 +47,8 @@ for ( 1..$generations ) {
 sub compute_wheel {
   my $population = shift;
   my $total_fitness;
-  map( $total_fitness += $_->{'fitness'}, @$population );
-  my @wheel = map( $_->{'fitness'}/$total_fitness, @$population);
+  map( $total_fitness += $fitness_of{$_}, @$population );
+  my @wheel = map( $fitness_of{$_}/$total_fitness, @$population);
   return @wheel;
 }
 
@@ -65,24 +64,19 @@ sub random_chromosome {
   for (1..$length) {
     $string .= (rand >0.5)?1:0;
   }
-  { string => $string,
-    fitness => undef };
+  $string;
 }
 
 sub mutate {
   my $chromosome = shift;
-  my $clone = { string => $chromosome->{'string'},
-		fitness => undef };
-  my $mutation_point = rand( length( $clone->{'string'} ));
-  substr($clone->{'string'}, $mutation_point, 1,
-	 ( substr($clone->{'string'}, $mutation_point, 1) eq 1 )?0:1 );
-  return $clone;
+  my $mutation_point = rand( length( $chromosome ));
+  substr($chromosome, $mutation_point, 1,
+	 ( substr($chromosome, $mutation_point, 1) eq 1 )?0:1 );
+  return $chromosome;
 }
 
 sub crossover {
-  my ($chrom_1, $chrom_2) = @_;
-  my $chromosome_1 = { string => $chrom_1->{'string'} };
-  my $chromosome_2 = { string => $chrom_2->{'string'} };
+  my ($chromosome_1, $chromosome_2) = @_;
   my $length = length( $chromosome_1 );
   my $xover_point_1 = int rand( $length -1 );
   my $xover_point_2 = int rand( $length -1 );
@@ -93,20 +87,20 @@ sub crossover {
   }
   $xover_point_2 = $xover_point_1 + 1 if ( $xover_point_2 == $xover_point_1 );
   my $swap_chrom = $chromosome_1;
-  substr($chromosome_1->{'string'}, $xover_point_1, $xover_point_2 - $xover_point_1 + 1,
-	 substr($chromosome_2->{'string'}, $xover_point_1, $xover_point_2 - $xover_point_1 + 1) );
-  substr($chromosome_2->{'string'}, $xover_point_1, $xover_point_2 - $xover_point_1 + 1,
-	 substr($swap_chrom->{'string'}, $xover_point_1, $xover_point_2 - $xover_point_1 + 1) );
+  substr($chromosome_1, $xover_point_1, $xover_point_2 - $xover_point_1 + 1,
+	 substr($chromosome_2, $xover_point_1, $xover_point_2 - $xover_point_1 + 1) );
+  substr($chromosome_2, $xover_point_1, $xover_point_2 - $xover_point_1 + 1,
+	 substr($swap_chrom, $xover_point_1, $xover_point_2 - $xover_point_1 + 1) );
   return ( $chromosome_1, $chromosome_2 );
 }
 
 sub compute_fitness {
   my $chromosome = shift;
   my $unos = 0;
-  for ( my $i = 0; $i < length($chromosome->{'string'}); $i ++ ) {
-      $unos += substr($chromosome->{'string'}, $i, 1 );
+  for ( my $i = 0; $i < length($chromosome); $i ++ ) {
+      $unos += substr($chromosome, $i, 1 );
   }
-  $chromosome->{'fitness'} = $unos;
+  $fitness_of{$chromosome} = $unos;
 }
 
 
