2bc19c045a148bd39a6e5a4516d456b8e3c96518
Juan J. Merelo <jmerelo@sheldon>
2010-09-27 14:36:22 +0200

    Improvement after transformation

--- ga-v0.lua
+++ ga-v0.lua
@@ -2,23 +2,32 @@
 
 -- Create a random chromosome
 function random_chromosome (length)
-   chromosome = ''
+   chromosome = {}
    for i=1,length  do
-      chromosome = chromosome .. ((math.random() >0.5) and "1" or "0") 
+      chromosome[i] = (math.random() >0.5) and 1 or 0 
    end
    return chromosome
 end
 
 -- Compare according to fitness
 function compare(a,b)
-   return fitness_of[b] < fitness_of[a]
+   return fitness_of[table.concat(b)] < fitness_of[table.concat(a)]
+end
+
+-- Deep copy array
+function copy_of(a)
+   local b = {}
+   for key, value in ipairs(a) do
+      b[key]=value
+   end
+   return b
 end
 
 -- Computes maxOnes fitness
 function compute_fitness (chromosome)
    ones = 0
-   for i=1,chromosome:len() do 
-      ones = ones + ( (chromosome.sub(i,i) == "1") and 1 or 0 )
+   for i=1,#chromosome do 
+      ones = ones + chromosome[i]
    end
    return ones
 end
@@ -35,26 +44,32 @@ end
 -- Mutate a single chromosome
 function mutate ( pool )
    for i=1,#pool do
-      mutation_point = math.random( pool[i]:len() )
-      temp = pool[i]
-      pool[i] = temp:sub(1,mutation_point-1)
-      pool[i] = pool[i] .. temp:sub(mutation_point,mutation_point)
-      pool[i] = pool[i] .. temp:sub(mutation_point+1,temp:len())
+      mutation_point = math.random( table.getn(pool[i] ))
+      pool[i][ mutation_point ]  =  ( (pool[i][mutation_point] == 1) and 0 or 1 )
    end
 end
 
 -- crossover 
 function crossover ( chrom1, chrom2 )
-   length = chrom1:len()
+   length = table.getn( chrom1 )
    xover_point = math.floor( math.random ( length - 1 ))
    range = 1 + math.floor( math.random ( length - xover_point ) )
-   new_chrom1 = chrom1:sub(1,xover_point-1)
-   new_chrom2 = chrom2:sub(1,xover_point-1)
-   new_chrom1 = new_chrom1 .. chrom2:sub(xover_point,xover_point+range)
-   new_chrom2 = new_chrom2 .. chrom1:sub(xover_point,xover_point+range)
-   new_chrom1 = new_chrom1 .. chrom1:sub(xover_point+range+1,length)
-   new_chrom2 = new_chrom2 .. chrom2:sub(xover_point+range+1,length)
-   return new_chrom1, new_chrom2
+   new_chrom1 = {}
+   new_chrom2 = {}
+   for i = 1,xover_point-1 do
+      new_chrom1[i] = chrom1[i]
+      new_chrom2[i] = chrom2[i]
+   end
+   for i = xover_point,xover_point+range do
+      new_chrom1[i] = chrom2[i]
+      new_chrom2[i] = chrom1[i]
+   end
+   for i = xover_point+range+1,length do
+      new_chrom1[i] = chrom1[i]
+      new_chrom2[i] = chrom2[i]
+   end
+--   print("X ".. table.concat(chrom1) .. " " .. table.concat(chrom2) .. "\n  " .. table.concat(new_chrom1).. " " .. table.concat(new_chrom2) )
+   return new_chrom1, new_chrom2 
 end
 
 -- Here goes the program
@@ -82,10 +97,11 @@ repeat
    total_fitness = 0;
    print( "Generation "..this_generation )
    for i=1,population_size do
-      if ( fitness_of[population[i]] == nil ) then
-	 fitness_of[population[i]] = compute_fitness( population[i] )
+      key = table.concat(population[i])
+      if ( fitness_of[key] == nil ) then
+	 fitness_of[key] = compute_fitness( population[i] )
       end
-      total_fitness = total_fitness + fitness_of[population[i]]
+      total_fitness = total_fitness + fitness_of[key]
    end
    table.sort( population, compare )
 
@@ -96,18 +112,19 @@ repeat
 ]]--
 
    for i=1,2 do
-      best[i] = population[i]  -- keep for later
+      best[i] = copy_of( population[i] ) -- keep for later
+--      print( "B", i, " ", table.concat(best[i] ))
    end
 
    wheel ={}
    for i=1,population_size do
-      wheel[i] = fitness_of[population[i]]/total_fitness
+      wheel[i] = fitness_of[table.concat(population[i])]/total_fitness
    end
    slots = spin( wheel, population_size )
 
    pool = {}
    index = 0
-   while #pool < population_size do
+   while #pool < population_size-1 do
       p = 1 + index % #slots
       index = index+1
       
@@ -116,11 +133,14 @@ repeat
 	 pool[#pool+1] = population[p]
       end
    end
+   
    population = {}
    mutate( pool )
+
    for i = 1,population_size/2-1 do
-      first = pool[math.random(#pool)];
-      second = pool[math.random(#pool)];
+      first = pool[math.random(table.getn(pool))];
+      second = pool[math.random(table.getn(pool))];
+      
       first_prime, second_prime = crossover( first, second)
       population[#population+1] = first_prime
       population[#population+1] = second_prime
@@ -129,6 +149,6 @@ repeat
    population[#population+1] = best[2]
    this_generation = this_generation + 1
 
-until ( this_generation > generations ) or (fitness_of[best[1]] >= chromosome_length ) 
+until ( this_generation > generations ) or (fitness_of[table.concat(best[1])] >= chromosome_length ) 
 profiler.stop()
-print( "Best\n\t"..best[1].." -> "..fitness_of[best[1]] )
+print( "Best\n\t"..table.concat(best[1]).." -> "..fitness_of[table.concat(best[1])] )
